4d24ee15c9ba778571b4ca5018bb42f130ab8ddf
tomato-101 <aungsone2022@gmail.com>
2023-04-21 22:36:21 +0630

    NODE-JS-PART-2

--- tut30.js
+++ tut30.js
@@ -6,4 +6,9 @@
 // 3. .gitignore name => name နေရာမှာ git မတင်ချင်တဲ့ file name 
 // 4. git status => file တွေပြန်ကြည့် 
 // 5. .env file က security အတွက်အရေးကြီးပါတယ် ဒါကြောင့် Git မတင်ပဲချန်ထားခဲ့သင့်ပါတယ်
-// 6. git add => git commit -m 'project name'
+// 6. readme.md for read me file (optional)
+// 7. git add . => git commit -m 'project name'
+// 8. git remote add origin (..github project link)
+// 9. git remote -v => git link ထည့်မယ့်ပုံစံကို စစ်ဆေးပါ
+// 10. git push -u origin master => permission တောင်းရင် allow လုပ်ပေးပါ
+// 11. done ပြရင်  upload တင်ခြင်းအောင်မြင်ပါပြီ
